e7ea1f6f8b06017540b050be24389cc4635e2af4
ZaneLesley <zanelesley@outlook.com>
2025-02-04 21:14:57 -0600

    feat(pages/database): Contiuned adding displaying to pages from the database

--- db/migrations/002_create_entity_type_table.sql
+++ db/migrations/002_create_entity_type_table.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS product_group
+CREATE TABLE IF NOT EXISTS entity_type
 (
-    product_id            INT PRIMARY KEY,
+    product_id            INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     store_id              INT         NOT NULL,
     product_category_name varchar(50) NOT NULL,
     FOREIGN KEY (store_id) REFERENCES store (store_id)
