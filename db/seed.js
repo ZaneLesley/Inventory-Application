687517331eee2be34ad7421cc051be180e530df0
ZaneLesley <zanelesley@outlook.com>
2025-02-06 01:05:30 -0600

    feat(seed): completed the ability to seed the items table

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,4 @@
 const {Client} = require('pg');
-const pool = require("./pool");
 require('dotenv').config({path: '../.env'});
 
 async function truncateAllTables(client) {
@@ -46,7 +45,7 @@ async function populateCategoriesTable(client, entity_name) {
             [entity_name, store_id]);
         const entity_type_id = rows.map(row => row.entity_type_id);
 
-        const categories = ['Fruit', 'Vegetable', 'Produce'];
+        const categories = ['Fruit', 'Vegetable', 'Meat'];
         const users = ['Bill', 'Bob', 'Joe'];
         if (entity_name === 'Product') {
             for (const category of categories) {
@@ -67,7 +66,26 @@ async function populateCategoriesTable(client, entity_name) {
 }
 
 async function populateItemTable(client) {
-    return undefined;
+    const categories = ['Fruit', 'Vegetable', 'Meat'];
+    const result = await client.query(`SELECT category_id
+                                       FROM categories
+                                       WHERE category_name = ANY ($1)`,
+        [categories]);
+    const category_ids = result.rows.map(row => row.category_id);
+    const item_names = [
+        'Strawberry', 'Celery', 'Steak',
+        'Watermelon', 'Broccoli', 'Pork',
+        'Kiwi', 'Cucumber', 'Beef'];
+    const item_prices = [
+        1.02, 1.43, 4.32,
+        1.92, 1.45, 3.23,
+        2.05, 0.99, 6.49];
+    await client.query(
+        `INSERT INTO item (category_id, item_name, item_price)
+         SELECT *
+         FROM UNNEST($1::int[], $2::text[], $3::decimal[])`,
+        [category_ids, item_names, item_prices]
+    );
 }
 
 async function main() {
