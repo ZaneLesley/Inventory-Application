f0f2505a4ccfe41837d4efb2f3905301a7dd4d1b
ZaneLesley <zanelesley@outlook.com>
2025-02-07 18:01:06 -0600

    feat(item form): added the item form, has no functionality.

--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -2,7 +2,8 @@ const {Router} = require("express");
 const storeController = require("../controllers/storeController");
 const storeRouter = Router();
 
-storeRouter.post("/update/:id/:entity", storeController.storeCategoryPost);
+storeRouter.post("/update/category/:id/:entity", storeController.storeCategoryPost);
+storeRouter.post("/update/item/:id/:entity", storeController.storeItemPost)
 storeRouter.get("/:id/:entity/:item", storeController.storeItemsGet);
 storeRouter.get("/:id/:entity/", storeController.storeEntityGet);
 storeRouter.get("/:id", storeController.storeGet);
